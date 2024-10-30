db2b163a2b21c69c4c60a9219e62cc87e2f7e505
MisokralPanovic <81387175+MisokralPanovic@users.noreply.github.com>
2024-10-30 19:04:03 -0400

    data cleanup

--- scripts/WT16_check-if-viral-genes-expressed.R
+++
@@ -1,8 +0,0 @@
-WT16 <- as.matrix(read.table('Data/WT16vsMock.counts', header = T))
-
-# filters out viral transcripts (only leaves stuff starting with ENS)
-nrow(WT16)
-WT16 <- WT16[!rownames(WT16) %like% "ENS", ]
-nrow(WT16)
-
-WT16
